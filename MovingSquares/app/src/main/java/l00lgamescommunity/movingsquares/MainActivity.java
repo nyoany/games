6f3171913fcd8f25eec537e0d1c204f5437e8d4a
tttppp <tttppp@users.noreply.github.com>
2017-07-15 17:21:26 +0100

    Fix imports.

--- MovingSquares/app/src/main/java/l00lgamescommunity/movingsquares/MainActivity.java
+++ MovingSquares/app/src/main/java/l00lgamescommunity/movingsquares/MainActivity.java
@@ -5,10 +5,17 @@ import java.util.List;
 import java.util.Random;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
